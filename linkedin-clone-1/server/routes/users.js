427a0f26263692b5eb61a0c364cec1f6f96ddc55
sobhralin <132080454+sobhralin@users.noreply.github.com>
2023-11-30 20:52:33 +0530

    Add files via upload

---
+++ linkedin-clone-1/server/routes/users.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+  getAllUsers
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+router.get("/", verifyToken, getAllUsers);
+
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
